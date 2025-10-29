cab21031330b2e4bd2948a2c5f41a267613e4402
zosqk21114 <zosqk21114@gmail.com>
2025-10-29 17:43:13 +0900

    Update main.py
    짤 추가

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if st.button("환영인사"):
     st.write(name+"님 안녕하세요!")
     st.balloons()
 st.image("https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg?semt=ais_hybrid&w=740&q=80")
-
+st.image("https://blog.kakaocdn.net/dna/CH4Ie/btqCSXeoDiS/AAAAAAAAAAAAAAAAAAAAAEjvz-VtAORzLplgUvKVyG2cCs_h0GYa0w0CR7zPAfDy/img.png?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&expires=1761922799&allow_ip=&allow_referer=&signature=MvWb8yd%2BjIAxoGiRtSUijNJxy4k%3D")
 st.success("성공!")
 st.warning("경고!")
 st.error("오류!")
